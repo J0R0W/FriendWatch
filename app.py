8ffb0c5cc4a80c3319b50520b4937930b73aef8f
J0R0W <ahnung_1@web.de>
2021-01-25 23:57:55 +0100

    Better Algorithm for Video Timings

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import os
 import random
 import string
+import time
+
 from werkzeug.utils import secure_filename
 from flask import Flask, render_template, request, json, session, abort, redirect, url_for
 from utility import RepeatedTimer
 
 app = Flask(__name__)
 app.secret_key = "iguzuzcsd89sd80f9z08whwucf03q2cd08hcwkjkjsdqq"
-videoPath = "static/video"
+maxDelaySeconds = 1
+
 adminTime = 4
 # Dictonary of All Avaible Rooms
 Rooms = {}
@@ -92,6 +95,8 @@ def videostatus():
 
     # If Video Already playing send current Play Time, else set current Position to Global position
     if videoEvent == 'play':
+        r.time = videotime
+        r.lastPlay = time.time()
         if r.status == 'play':
             return json.dumps({'time': r.time, "updated": True, 'status': r.status})
         else:
@@ -99,26 +104,32 @@ def videostatus():
     if videoEvent == 'seeked':
         # update the Time for the Video
         r.time = float(videotime)
+        r.lastPlay = time.time()
     elif videoEvent == "pause":
         r.status = videoEvent
+        r.time = videotime
     return json.dumps({'status': 'OK', 'user': "Test"});
 
 
-# Method for request to read Video state and update client
+# Method for request to read Video state and update the client
 @app.route('/updateVideo', methods=['POST'])
 def updatetime():
     videotime = request.form['time']
     videoid = request.form['id']
     try:
         r = Rooms[videoid]
-
-        # if abs(r.lastPlay + float(videotime) - time.time()) > 1:
-        if abs(r.time - float(videotime)) > 1:
-            return json.dumps({'time': r.time, "updated": True, 'status': r.status})
-        return json.dumps({"noupdate": False, 'status': r.status})
     except:
         # The Room Code was not found
         return "Bad Room-Code"
+
+    # if abs(r.lastPlay + float(videotime) - time.time()) > 1:
+    time_pos_of_video = abs(calc_where_the_video_should_be(videoid))
+    print("Video should be at:" + str(time_pos_of_video))
+    if abs(time_pos_of_video - float(videotime)) > maxDelaySeconds and r.status == "play":
+        print("Video Delayed:" + str(abs(time_pos_of_video - float(videotime))))
+        # returnTime = (float(r.time) + time_pos_of_video)
+        return json.dumps({'time': time_pos_of_video, "updated": True, 'status': r.status})
+    return json.dumps({"updated": False, 'status': r.status})
 
 
 @app.route("/startwatch")
@@ -129,10 +140,10 @@ def startwatch():
         path = os.path.join(path, "static")
         path = os.path.join(path, "video")
         list_of_files = {}
-        #Put every File in Video Folder in the list
+        # Put every File in Video Folder in the list
         for filename in os.listdir(path):
             list_of_files[filename] = "/static/video" + filename
-        #return the Template with the List, of all files in the Video folder
+        # return the Template with the List, of all files in the Video folder
         return render_template("startwatch.html", videos=list_of_files)
     else:
         abort(403)
@@ -144,6 +155,14 @@ def calculateTime():
     for key, value in Rooms.items():
         if value.status == "play":
             value.time += 0.25
+
+
+def calc_where_the_video_should_be(raumID):
+    currentTime = time.time()
+    r = Rooms[raumID]
+    timepos = float(r.lastPlay) - float(r.time)
+    timeWhereVideoShouldBe = abs(timepos - currentTime)
+    return timeWhereVideoShouldBe
 
 
 # Simple Login Method. Only One Username Exist.
@@ -159,7 +178,5 @@ def login():
         return render_template("login.html")
 
 
-# The Timer which calls the calculateTimeMethod
-ti = RepeatedTimer(0.25, calculateTime)
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=1337)
